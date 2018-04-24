581cb0b5de021b0509ff2c069131443326a71002
Dmitry Savchenko <lux2057@gmail.com>
2018-04-24 12:44:06 +0300

    Comments were added for CacheManager

--- Kongrevsky.Libraries/Infrastructure/Infrastructure.CacheManager/ICacheManager.cs
+++ Kongrevsky.Libraries/Infrastructure/Infrastructure.CacheManager/ICacheManager.cs
@@ -9,12 +9,46 @@
 
     public interface ICacheManager
     {
+        /// <summary>
+        /// Sets value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="userKey"></param>
+        /// <param name="key"></param>
+        /// <param name="obj"></param>
+        /// <param name="expiration"></param>
         void SetValue<T>(string userKey, string key, T obj, TimeSpan? expiration = null);
 
+        /// <summary>
+        /// Returns existing value or adds it and returns
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="userKey"></param>
+        /// <param name="key"></param>
+        /// <param name="expr"></param>
+        /// <param name="expiration"></param>
+        /// <returns></returns>
         T GetOrSetValue<T>(string userKey, string key, Func<T> expr, TimeSpan? expiration = null);
 
+        /// <summary>
+        /// Async returns existing value or adds it and returns
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="userKey"></param>
+        /// <param name="key"></param>
+        /// <param name="expr"></param>
+        /// <param name="expiration"></param>
+        /// <returns></returns>
         Task<T> GetOrSetValueAsync<T>(string userKey, string key, Func<Task<T>> expr, TimeSpan? expiration = null);
 
+        /// <summary>
+        /// Tries to get value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="userKey"></param>
+        /// <param name="hash"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         bool TryGetValue<T>(string userKey, string hash, out T value);
     }
 }
