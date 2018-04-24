66290aa38de9aa328e7424da7c6a96b54117ae8e
Dmitry Savchenko <lux2057@gmail.com>
2018-04-24 13:03:40 +0300

    Extenstions for default managers' registry were added

---
+++ Kongrevsky.Libraries/Infrastructure/Infrastructure.ActiveDirectoryManager/ActiveDirectoryExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Kongrevsky.Infrastructure.ActiveDirectoryManager
+{
+    #region << Using >>
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    #endregion
+
+    public static class ActiveDirectoryExtensions
+    {
+        public static void RegisterDefaultActiveDirectoryManager(this IServiceCollection services, bool isFake)
+        {
+            if (isFake)
+                services.AddTransient<IActiveDirectoryManager, FakeActiveDirectoryManager>();
+            else
+                services.AddTransient<IActiveDirectoryManager, ActiveDirectoryManager>();
+        }
+    }
+}
---
+++ Kongrevsky.Libraries/Infrastructure/Infrastructure.FileManager/FileManagerExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Kongrevsky.Infrastructure.FileManager
+{
+    #region << Using >>
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    #endregion
+
+    public static class FileManagerExtensions
+    {
+        public static void RegisterDefaultFileManager(this IServiceCollection services)
+        {
+            services.AddSingleton<IFileManager, FileManager>();
+        }
+    }
+}
