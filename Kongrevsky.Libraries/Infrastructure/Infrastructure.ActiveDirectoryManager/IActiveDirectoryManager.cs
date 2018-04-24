3ad21fba936999fd2dde300e12d22d1e8aec1c19
Dmitry Savchenko <lux2057@gmail.com>
2018-04-24 12:41:43 +0300

    Comments were added for ActiveDirectoryManager

--- Kongrevsky.Libraries/Infrastructure/Infrastructure.ActiveDirectoryManager/IActiveDirectoryManager.cs
+++ Kongrevsky.Libraries/Infrastructure/Infrastructure.ActiveDirectoryManager/IActiveDirectoryManager.cs
@@ -10,16 +10,46 @@ namespace Kongrevsky.Infrastructure.ActiveDirectoryManager
 
     public interface IActiveDirectoryManager
     {
+        /// <summary>
+        /// Detects if connection is alive
+        /// </summary>
+        /// <returns></returns>
         bool CheckConnection();
 
+        /// <summary>
+        /// Async returns an user by specified searching term
+        /// </summary>
+        /// <param name="search"></param>
+        /// <returns></returns>
         Task<ADUser> FindUserAsync(string username);
 
+        /// <summary>
+        /// Async returns an user by specified email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
         Task<ADUser> GetUserByEmailAsync(string email);
 
+        /// <summary>
+        /// Async returns an user by specified username
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
         Task<ADUser> GetUserByUsernameAsync(string username);
 
+        /// <summary>
+        /// Async returns users by specified searching term
+        /// </summary>
+        /// <param name="search"></param>
+        /// <returns></returns>
         Task<List<ADUser>> GetUsersAsync(string search);
 
+        /// <summary>
+        /// Async detects if specified credentials are valid
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         Task<bool> ValidateUserCredentialsAsync(string username, string password);
     }
 }
