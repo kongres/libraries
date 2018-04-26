45994b2356a8358cc068ab682fb612ddd6f87da0
Dmitry Savchenko <lux2057@gmail.com>
2018-04-26 13:22:08 +0300

    Resources were prepared to MyGet publishing

--- Kongrevsky.Libraries/Resources/Resources.Location/Models/State.cs
+++ Kongrevsky.Libraries/Resources/Resources.Location/Models/State.cs
@@ -1,8 +1,13 @@
-﻿namespace Resources.Location.Models
+﻿namespace Kongrevsky.Resources.Location.Models
 {
     public class State
     {
+        #region Properties
+
         public string Name { get; set; }
+
         public string CountryId { get; set; }
+
+        #endregion
     }
 }
