a57918ede42dc1f07faea23ab4e43b93a4d0c864
Igor Valikovsky <kongres007@gmail.com>
2018-08-17 10:00:30 +0300

    Added SessionUtils for Web

---
+++ Kongrevsky.Libraries/Utilities/Utilities.Web/SessionUtils.cs
@@ -0,0 +1,19 @@
+﻿namespace Kongrevsky.Utilities.Web
+{
+    using Microsoft.AspNetCore.Http;
+    using Newtonsoft.Json;
+
+    public static class SessionUtils
+    {
+        public static T Get<T>(this ISession session, string key)
+        {
+            var data = session.GetString(key);
+            return string.IsNullOrEmpty(data) ? default(T) : JsonConvert.DeserializeObject<T>(data);
+        }
+
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+    }
+}
