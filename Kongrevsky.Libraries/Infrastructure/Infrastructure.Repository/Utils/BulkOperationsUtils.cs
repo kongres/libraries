73ba0841a99ceee9f3a3b27b13af1c963f509ddb
Igor Valikovsky <kongres007@gmail.com>
2018-10-16 13:16:21 +0300

    Fix

--- Kongrevsky.Libraries/Infrastructure/Infrastructure.Repository/Utils/BulkOperationsUtils.cs
+++ Kongrevsky.Libraries/Infrastructure/Infrastructure.Repository/Utils/BulkOperationsUtils.cs
@@ -3,13 +3,9 @@
     #region << Using >>
 
     using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
-    using System.Linq.Expressions;
-    using System.Reflection;
     using Kongrevsky.Utilities.Expression;
-    using Kongrevsky.Utilities.Object;
     using SqlBulkTools;
     using SqlBulkTools.BulkCopy;
 
@@ -40,47 +36,18 @@
                 if (!attrs.Any())
                     continue;
 
-                // todo: need to recheck and refactor this code
                 var columnName = ExpressionUtils.ToLambda<T, object>(property.Name);
-                if (!bulk.ContainsColumns(columnName))
+
+                try
+                {
                     bulk.RemoveColumn(columnName);
+                }
+                catch (Exception e)
+                {
+                }
             }
 
             return bulk;
-        }
-
-        public static bool ContainsColumns<T>(this AbstractColumnSelection<T> bulk, Expression<Func<T, object>> columnName)
-        {
-            var propertyName = GetPropertyName(columnName);
-            var prop = typeof(AbstractColumnSelection<T>).GetProperty("_column", BindingFlags.NonPublic | BindingFlags.Instance);
-            var getter = prop.GetGetMethod(nonPublic: true);
-            var _columns = (HashSet<string>)getter.Invoke(bulk, null);
-
-            return _columns.Contains(propertyName);
-        }
-
-        /// <summary>
-        /// Original <see cref="BulkOperationsHelper.GetPropertyName"/>
-        /// </summary>
-        /// <param name="method"></param>
-        /// <returns></returns>
-        private static string GetPropertyName(Expression method)
-        {
-            LambdaExpression lambdaExpression = method as LambdaExpression;
-            if (lambdaExpression == null)
-                throw new ArgumentNullException(nameof(method));
-            MemberExpression memberExpression = (MemberExpression)null;
-            if (lambdaExpression.Body.NodeType == ExpressionType.Convert)
-            {
-                memberExpression = ((UnaryExpression)lambdaExpression.Body).Operand as MemberExpression;
-                if ((memberExpression != null ? memberExpression.Expression.Type.GetCustomAttribute(typeof(ComplexTypeAttribute)) : (Attribute)null) != null && memberExpression.Expression is MemberExpression)
-                    return string.Format("{0}_{1}", (object)((MemberExpression)memberExpression.Expression).Member.Name, (object)memberExpression.Member.Name);
-            }
-            else if (lambdaExpression.Body.NodeType == ExpressionType.MemberAccess)
-                memberExpression = lambdaExpression.Body as MemberExpression;
-            if (memberExpression == null)
-                throw new ArgumentException(nameof(method));
-            return memberExpression.Member.Name;
         }
     }
 }
