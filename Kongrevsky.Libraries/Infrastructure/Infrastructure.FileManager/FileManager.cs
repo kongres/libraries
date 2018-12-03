0e9e7c835ccc5fb4707a6328cfaca29b8b2cc902
Igor Valikovsky <kongres007@gmail.com>
2018-12-03 18:34:45 +0300

    Added Impersonation to FileManager

--- Kongrevsky.Libraries/Infrastructure/Infrastructure.FileManager/FileManager.cs
+++ Kongrevsky.Libraries/Infrastructure/Infrastructure.FileManager/FileManager.cs
@@ -6,6 +6,7 @@
     using System.IO;
     using Kongrevsky.Infrastructure.FileManager.Models;
     using Kongrevsky.Infrastructure.FileManager.Utils;
+    using Kongrevsky.Infrastructure.Impersonator;
     using Kongrevsky.Utilities.Common;
     using Microsoft.Extensions.Options;
 
@@ -13,132 +14,12 @@
 
     public class FileManager : IFileManager
     {
-        #region Properties
-
-        private FileManagerOptions FileManagerOptions { get; set; }
-
-        #endregion
-
-        #region Interface Implementations
-
-        public FileManager(IOptions<FileManagerOptions> fileManagerOptions)
-        {
-            FileManagerOptions = fileManagerOptions.Value ?? throw new ArgumentNullException(nameof(fileManagerOptions));
-            if (string.IsNullOrEmpty(FileManagerOptions.RootPath))
-                throw new ArgumentNullException(nameof(FileManagerOptions.RootPath));
-            Directory.CreateDirectory(FileManagerOptions.RootPath);
-        }
-
-        public FileObject GetFile(string fileId, bool compress = false, string encryptPassword = null)
-        {
-            var filePath = _getFilePath(fileId);
-
-            if (!FileUtils.IsFileExists(filePath))
-                return new FileObject(Stream.Null, fileId);
-
-            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Delete | FileShare.Read, 4096, true);
-
-            try
-            {
-                if (string.IsNullOrEmpty(encryptPassword))
-                    if (compress)
-                        using (fileStream)
-                        {
-                            return new FileObject(FileUtils.DecompressFile(fileStream), fileId);
-                        }
-                    else
-                        return new FileObject(fileStream, fileId);
-                else
-                    using (fileStream)
-                    {
-                        var decryptFile = FileUtils.FileDecrypt(fileStream, encryptPassword);
-
-                        if (compress)
-                            using (decryptFile)
-                            {
-                                return new FileObject(FileUtils.DecompressFile(decryptFile), fileId);
-                            }
-                        else
-                            return new FileObject(decryptFile, fileId);
-                    }
-            }
-            catch (Exception e)
-            {
-                return new FileObject(Stream.Null, fileId);
-            }
-        }
-
-        public bool SaveFile(string fileId, Stream stream, bool compress = false, string encryptPassword = null)
-        {
-            try
-            {
-                var directoryPath = _getDirectoryPath(fileId);
-
-                if (compress)
-                {
-                    using (var compressedStream = FileUtils.CompressFile(stream))
-                    {
-                        if (string.IsNullOrEmpty(encryptPassword))
-                            FileUtils.SaveFile(directoryPath, fileId, compressedStream);
-                        else
-                            using (var enryptStream = FileUtils.FileEncrypt(compressedStream, encryptPassword))
-                            {
-                                FileUtils.SaveFile(directoryPath, fileId, enryptStream);
-                            }
-                    }
-                }
-                else
-                {
-                    if (string.IsNullOrEmpty(encryptPassword))
-                        FileUtils.SaveFile(directoryPath, fileId, stream);
-                    else
-                        using (var enryptStream = FileUtils.FileEncrypt(stream, encryptPassword))
-                        {
-                            FileUtils.SaveFile(directoryPath, fileId, enryptStream);
-                        }
-                }
-
-                return true;
-            }
-            catch (Exception e)
-            {
-                return false;
-            }
-        }
-
-        public bool CheckFileExists(string fileId)
-        {
-            var filePath = _getFilePath(fileId);
-            return File.Exists(filePath);
-        }
-
-        public void DeleteFile(string fileId)
-        {
-            RetryUtils.Do(() => FileUtils.DeleteDirectory(_getDirectoryPath(fileId)), TimeSpan.FromSeconds(1));
-        }
-
-        public bool TryDeleteFile(string fileId)
-        {
-            try
-            {
-                DeleteFile(fileId);
-                return true;
-            }
-            catch (Exception e)
-            {
-                // log
-                return false;
-            }
-        }
-
-        #endregion
-
         private string _getDirectoryPath(string fileId)
         {
             if (string.IsNullOrEmpty(fileId))
                 throw new ArgumentException("FileId can't be null or empty", nameof(fileId));
 
-            var directoryPath = Path.Combine(FileManagerOptions.RootPath, fileId);
+            var directoryPath = Path.Combine(_fileManagerOptions.RootPath, fileId);
             return directoryPath;
         }
 
@@ -147,5 +28,167 @@
             var directoryPath = _getDirectoryPath(fileId);
             return Path.Combine(directoryPath, fileId);
         }
+
+        #region Properties
+
+        private FileManagerOptions _fileManagerOptions { get; }
+
+        private UserCredentials _userCredentialsImpersonation { get; }
+
+        #endregion
+
+        #region Interface Implementations
+
+        public FileManager(IOptions<FileManagerOptions> fileManagerOptions)
+        {
+            _fileManagerOptions = fileManagerOptions.Value ?? throw new ArgumentNullException(nameof(fileManagerOptions));
+            if (string.IsNullOrEmpty(_fileManagerOptions.RootPath))
+                throw new ArgumentNullException(nameof(_fileManagerOptions.RootPath));
+
+            if ((_fileManagerOptions.Impersonation ?? (_fileManagerOptions.Impersonation = new ImpersonationOptions())).IsImpersonationEnabled)
+            {
+                _userCredentialsImpersonation = new UserCredentials(_fileManagerOptions.Impersonation.ImpersonationDomain, _fileManagerOptions.Impersonation.ImpersonationUsername, _fileManagerOptions.Impersonation.ImpersonationPassword);
+                Impersonation.RunAsUser(_userCredentialsImpersonation,
+                                        () => { Directory.CreateDirectory(_fileManagerOptions.RootPath); },
+                                        LogonType.Interactive,
+                                        LogonProvider.Default);
+            }
+            else
+            {
+                Directory.CreateDirectory(_fileManagerOptions.RootPath);
+            }
+        }
+
+        public FileObject GetFile(string fileId, bool compress = false, string encryptPassword = null)
+        {
+            FileObject getFile()
+            {
+                var filePath = _getFilePath(fileId);
+
+                if (!FileUtils.IsFileExists(filePath))
+                    return new FileObject(Stream.Null, fileId);
+
+                var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Delete | FileShare.Read, 4096, true);
+
+                try
+                {
+                    if (string.IsNullOrEmpty(encryptPassword))
+                        if (compress)
+                            using (fileStream)
+                            {
+                                return new FileObject(FileUtils.DecompressFile(fileStream), fileId);
+                            }
+                        else
+                            return new FileObject(fileStream, fileId);
+                    else
+                        using (fileStream)
+                        {
+                            var decryptFile = FileUtils.FileDecrypt(fileStream, encryptPassword);
+
+                            if (compress)
+                                using (decryptFile)
+                                {
+                                    return new FileObject(FileUtils.DecompressFile(decryptFile), fileId);
+                                }
+                            else
+                                return new FileObject(decryptFile, fileId);
+                        }
+                }
+                catch (Exception e)
+                {
+                    return new FileObject(Stream.Null, fileId);
+                }
+            }
+
+            return _fileManagerOptions.Impersonation.IsImpersonationEnabled ? Impersonation.RunAsUser(_userCredentialsImpersonation, getFile, LogonType.Interactive, LogonProvider.Default) : getFile();
+        }
+
+        public bool SaveFile(string fileId, Stream stream, bool compress = false, string encryptPassword = null)
+        {
+            bool saveFile()
+            {
+                try
+                {
+                    var directoryPath = _getDirectoryPath(fileId);
+
+                    if (compress)
+                    {
+                        using (var compressedStream = FileUtils.CompressFile(stream))
+                        {
+                            if (string.IsNullOrEmpty(encryptPassword))
+                                FileUtils.SaveFile(directoryPath, fileId, compressedStream);
+                            else
+                                using (var encryptStream = FileUtils.FileEncrypt(compressedStream, encryptPassword))
+                                {
+                                    FileUtils.SaveFile(directoryPath, fileId, encryptStream);
+                                }
+                        }
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(encryptPassword))
+                            FileUtils.SaveFile(directoryPath, fileId, stream);
+                        else
+                            using (var encryptStream = FileUtils.FileEncrypt(stream, encryptPassword))
+                            {
+                                FileUtils.SaveFile(directoryPath, fileId, encryptStream);
+                            }
+                    }
+
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+
+            return _fileManagerOptions.Impersonation.IsImpersonationEnabled ? Impersonation.RunAsUser(_userCredentialsImpersonation, saveFile, LogonType.Interactive, LogonProvider.Default) : saveFile();
+        }
+
+        public bool CheckFileExists(string fileId)
+        {
+            bool checkFileExists()
+            {
+                var filePath = _getFilePath(fileId);
+                return File.Exists(filePath);
+            }
+
+            return _fileManagerOptions.Impersonation.IsImpersonationEnabled ? Impersonation.RunAsUser(_userCredentialsImpersonation, checkFileExists, LogonType.Interactive, LogonProvider.Default) : checkFileExists();
+        }
+
+        public void DeleteFile(string fileId)
+        {
+            void deleteFile()
+            {
+                RetryUtils.Do(() => FileUtils.DeleteDirectory(_getDirectoryPath(fileId)), TimeSpan.FromSeconds(1));
+            }
+
+            if (_fileManagerOptions.Impersonation.IsImpersonationEnabled)
+                Impersonation.RunAsUser(_userCredentialsImpersonation, deleteFile);
+            else
+                deleteFile();
+        }
+
+        public bool TryDeleteFile(string fileId)
+        {
+            bool tryDeleteFile()
+            {
+                try
+                {
+                    DeleteFile(fileId);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    // log
+                    return false;
+                }
+            }
+
+            return _fileManagerOptions.Impersonation.IsImpersonationEnabled ? Impersonation.RunAsUser(_userCredentialsImpersonation, tryDeleteFile, LogonType.Interactive, LogonProvider.Default) : tryDeleteFile();
+        }
+
+        #endregion
     }
 }
--- Kongrevsky.Libraries/Infrastructure/Infrastructure.FileManager/Models/FileManagerOptions.cs
+++ Kongrevsky.Libraries/Infrastructure/Infrastructure.FileManager/Models/FileManagerOptions.cs
@@ -2,10 +2,19 @@
 {
     public class FileManagerOptions
     {
-        #region Properties
-
         public string RootPath { get; set; }
 
-        #endregion
+        public ImpersonationOptions Impersonation { get; set; } = new ImpersonationOptions();
+    }
+
+    public class ImpersonationOptions
+    {
+        public bool IsImpersonationEnabled { get; set; } = false;
+
+        public string ImpersonationDomain { get; set; }
+
+        public string ImpersonationUsername { get; set; }
+
+        public string ImpersonationPassword { get; set; }
     }
 }
