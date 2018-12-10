d39f45ce6c45ae3c194509d2bfc3db33fadb4ea3
Igor Valikovsky <kongres007@gmail.com>
2018-12-10 17:10:45 +0300

    Added DI for FileManager as separated lib

---
+++ Kongrevsky.Libraries/Infrastructure/Kongrevsky.Infrastructure.FileManager.DependencyInjection/FileManagerExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Kongrevsky.Infrastructure.FileManager
+{
+    #region << Using >>
+
+    using System;
+    using Kongrevsky.Infrastructure.FileManager.Models;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    #endregion
+
+    public static class FileManagerExtensions
+    {
+        public static void AddKongrevskyFileManager(this IServiceCollection services, IConfigurationSection configurationSection)
+        {
+            services.AddSingleton<IFileManager>(provider => new FileManager(configurationSection.Get<FileManagerOptions>()));
+        }
+
+        public static void AddKongrevskyFileManager(this IServiceCollection services, Func<IConfigurationSection> configurationSection)
+        {
+            services.AddKongrevskyFileManager(configurationSection());
+        }
+
+        public static void AddKongrevskyFileManager(this IServiceCollection services, Action<FileManagerOptions> configurationSection)
+        {
+            var options = new FileManagerOptions();
+            configurationSection(options);
+            services.AddSingleton<IFileManager>(provider => new FileManager(options));
+        }
+    }
+}
