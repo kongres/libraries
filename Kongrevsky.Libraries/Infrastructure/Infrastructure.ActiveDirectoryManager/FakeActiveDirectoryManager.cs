efb02cd412ae998fb79bb83bedc8218cfbb52781
Igor Valikovsky <kongres007@gmail.com>
2019-01-29 11:44:24 +0300

    Removed static users from AD manager

--- Kongrevsky.Libraries/Infrastructure/Infrastructure.ActiveDirectoryManager/FakeActiveDirectoryManager.cs
+++ Kongrevsky.Libraries/Infrastructure/Infrastructure.ActiveDirectoryManager/FakeActiveDirectoryManager.cs
@@ -19,24 +19,12 @@
 
         private ActiveDirectoryOptions _activeDirectoryOptions { get; }
 
-        private IEnumerable<ADUser> adUsers => new List<ADUser>()
-                                               {
-                                                       new ADUser() { Email = "test_admin@transperfect.com", Username = "test_admin", FirstName = "Admin", LastName = "System", IsActive = true },
-                                                       new ADUser() { Email = "test_carol@transperfect.com", Username = "test_carol", FirstName = "Carol", LastName = "Rice", IsActive = true },
-                                                       new ADUser() { Email = "test_cristina@transperfect.com", Username = "test_cristina", FirstName = "Cristina", LastName = "Obrien", IsActive = true },
-                                                       new ADUser() { Email = "test_debra@transperfect.com", Username = "test_debra", FirstName = "Debra", LastName = "Tate", IsActive = true },
-                                                       new ADUser() { Email = "test_arlene@transperfect.com", Username = "test_arlene", FirstName = "Arlene", LastName = "Curry", IsActive = false },
-                                                       new ADUser() { Email = "test_erika@transperfect.com", Username = "test_erika", FirstName = "Erika", LastName = "Higgins", IsActive = true },
-                                                       new ADUser() { Email = "test_leo@transperfect.com", Username = "test_leo", FirstName = "Leo", LastName = "Burke", IsActive = false },
-                                                       new ADUser() { Email = "test_louis@transperfect.com", Username = "test_louis", FirstName = "Louis", LastName = "Henderson", IsActive = true },
-                                                       new ADUser() { Email = "test_shawna@transperfect.com", Username = "test_shawna", FirstName = "Shawna", LastName = "Montgomery", IsActive = true },
-                                                       new ADUser() { Email = "test_lynette@transperfect.com", Username = "test_lynette", FirstName = "Lynette", LastName = "Miller", IsActive = true },
-                                                       new ADUser() { Email = "test_israel@transperfect.com", Username = "test_israel", FirstName = "Israel", LastName = "Hopkins", IsActive = true },
-                                                       new ADUser() { Email = "test_perkins@transperfect.com", Username = "test_perkins", FirstName = "Austin", LastName = "Perkins", IsActive = true },
-                                                       new ADUser() { Email = "test_padilla@transperfect.com", Username = "test_padilla", FirstName = "Everett", LastName = "Padilla", IsActive = true },
-                                                       new ADUser() { Email = "test_darnell@transperfect.com", Username = "test_darnell", FirstName = "Darnell", LastName = "Wood", IsActive = true },
-                                                       new ADUser() { Email = "test_marcos@transperfect.com", Username = "test_marcos", FirstName = "Marcos", LastName = "Simpson", IsActive = true }
-                                               };
+        private static IEnumerable<ADUser> adUsers { get; set; }
+
+        public static void SeedAdUsers(IEnumerable<ADUser> users)
+        {
+            adUsers = users.ToList();
+        }
 
         #endregion
 
