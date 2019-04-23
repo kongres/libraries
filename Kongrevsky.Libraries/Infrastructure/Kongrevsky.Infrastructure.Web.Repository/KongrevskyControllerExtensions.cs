18e512dd6036b8fa5a543471196c6907f79c33f4
Igor Valikovsky <kongres007@gmail.com>
2019-04-23 14:11:41 +0300

    Update KongrevskyControllerExtensions

--- Kongrevsky.Libraries/Infrastructure/Kongrevsky.Infrastructure.Web.Repository/KongrevskyControllerExtensions.cs
+++ Kongrevsky.Libraries/Infrastructure/Kongrevsky.Infrastructure.Web.Repository/KongrevskyControllerExtensions.cs
@@ -11,12 +11,11 @@
 
     public static class KongrevskyControllerExtensions
     {
-        public static OkObjectResult OkPaging<TController, TFilter, TItem>(this TController controller, TFilter filter)
+        public static OkObjectResult OkPaging<TController, TItem>(this TController controller, RepositoryPagingModel<TItem> filter)
                 where TController : Controller
-                where TFilter : RepositoryPagingModel<TItem>
                 where TItem : class
         {
-            return filter.LoadProperties?.Any() ?? false ? new OkObjectResult(RepositoryPagingModelUtils.ExcludeIgnoredPropertiesPagingModel<TFilter, TItem>(filter, filter.LoadProperties, typeof(TItem))) : controller.Ok(filter);
+            return filter.LoadProperties?.Any() ?? false ? new OkObjectResult(RepositoryPagingModelUtils.ExcludeIgnoredPropertiesPagingModel(filter, filter.LoadProperties, typeof(TItem))) : controller.Ok(filter);
         }
     }
 }
