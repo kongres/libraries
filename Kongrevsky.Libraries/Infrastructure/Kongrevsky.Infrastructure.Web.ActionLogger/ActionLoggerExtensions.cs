edfa7687c6db7f670992b8d8363fea8d67efc96c
Igor Valikovsky <kongres007@gmail.com>
2019-05-07 19:00:08 +0300

    Update ActionLoggerExtensions.cs

--- Kongrevsky.Libraries/Infrastructure/Kongrevsky.Infrastructure.Web.ActionLogger/ActionLoggerExtensions.cs
+++ Kongrevsky.Libraries/Infrastructure/Kongrevsky.Infrastructure.Web.ActionLogger/ActionLoggerExtensions.cs
@@ -5,6 +5,7 @@
     using System;
     using Kongrevsky.Infrastructure.Web.ActionLogger.Models;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Options;
 
     #endregion
 
@@ -13,7 +14,13 @@
         public static IServiceCollection AddActionLogger(this IServiceCollection services, Action<ActionLoggerOptions> setupAction = null)
         {
             services.Configure(setupAction ?? (Action<ActionLoggerOptions>)(opts => { }));
-            services.AddSingleton<IActionLogger, ActionLogger>();
+            services.AddSingleton<IActionLogger>(provider =>
+                                                 {
+                                                     var actionLogger = new ActionLogger();
+                                                     var actionLoggerOptions = provider.GetService<IOptions<ActionLoggerOptions>>();
+                                                     actionLogger.SetOptions(actionLoggerOptions.Value);
+                                                     return actionLogger;
+                                                 });
             return services;
         }
     }
