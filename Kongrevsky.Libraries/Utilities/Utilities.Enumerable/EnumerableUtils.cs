9c16dc172f89b37a76d7392fa23b40d4a18d8a5f
Igor Valikovsky <kongres007@gmail.com>
2019-06-12 15:27:51 +0300

    Added DistinctBy for Enumerable

--- Kongrevsky.Libraries/Utilities/Utilities.Enumerable/EnumerableUtils.cs
+++ Kongrevsky.Libraries/Utilities/Utilities.Enumerable/EnumerableUtils.cs
@@ -295,5 +295,21 @@
         {
             return !lookupList.Except(containingList).Any();
         }
+
+        /// <summary>Returns distinct elements from a sequence by using Key Selector</summary>
+        /// <returns>An <see cref="T:System.Collections.Generic.IEnumerable`1" /> that contains distinct elements from the source sequence.</returns>
+        /// <param name="source">The sequence to remove duplicate elements from.</param>
+        /// <param name="keySelector">Key Selector</param>
+        /// <typeparam name="T">The type of the elements of <paramref name="source" />.</typeparam>
+        /// <typeparam name="TKey">The type of Key Selector</typeparam>
+        public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
+        {
+            var seenKeys = new HashSet<TKey>();
+            foreach (var element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                    yield return element;
+            }
+        }
     }
 }
