1474b395a4750679b74bfa6a82df481ac35c9356
Igor Valikovsky <kongres007@gmail.com>
2019-11-15 08:09:58 +0300

    Update KongrevskyDbContext.cs

--- Kongrevsky.Libraries/Infrastructure/Infrastructure.Repository/KongrevskyDbContext.cs
+++ Kongrevsky.Libraries/Infrastructure/Infrastructure.Repository/KongrevskyDbContext.cs
@@ -24,43 +24,82 @@
         }
 
         public KongrevskyDbContext(IServiceProvider serviceProvider)
-                : base(serviceProvider) { Configure(); }
+                : base(serviceProvider)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(DbCompiledModel model)
-                : base(model) { Configure(); }
+                : base(model)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(string nameOrConnectionString)
-                : base(nameOrConnectionString) { Configure(); }
+                : base(nameOrConnectionString)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(DbConnection existingConnection, bool contextOwnsConnection)
-                : base(existingConnection, contextOwnsConnection) { Configure(); }
+                : base(existingConnection, contextOwnsConnection)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(ObjectContext objectContext, bool dbContextOwnsObjectContext)
-                : base(objectContext, dbContextOwnsObjectContext) { Configure(); }
+                : base(objectContext, dbContextOwnsObjectContext)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(string nameOrConnectionString, DbCompiledModel model)
-                : base(nameOrConnectionString, model) { Configure(); }
+                : base(nameOrConnectionString, model)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(DbConnection existingConnection, DbCompiledModel model, bool contextOwnsConnection)
-                : base(existingConnection, model, contextOwnsConnection) { Configure(); }
+                : base(existingConnection, model, contextOwnsConnection)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(IServiceProvider serviceProvider, DbCompiledModel model)
-                : base(serviceProvider, model) { Configure(); }
+                : base(serviceProvider, model)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(IServiceProvider serviceProvider, string nameOrConnectionString)
-                : base(serviceProvider, nameOrConnectionString) { Configure(); }
+                : base(serviceProvider, nameOrConnectionString)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(IServiceProvider serviceProvider, DbConnection existingConnection, bool contextOwnsConnection)
-                : base(serviceProvider, existingConnection, contextOwnsConnection) { Configure(); }
+                : base(serviceProvider, existingConnection, contextOwnsConnection)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(IServiceProvider serviceProvider, ObjectContext objectContext, bool dbContextOwnsObjectContext)
-                : base(serviceProvider, objectContext, dbContextOwnsObjectContext) { Configure(); }
+                : base(serviceProvider, objectContext, dbContextOwnsObjectContext)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(IServiceProvider serviceProvider, string nameOrConnectionString, DbCompiledModel model)
-                : base(serviceProvider, nameOrConnectionString, model) { Configure(); }
+                : base(serviceProvider, nameOrConnectionString, model)
+        {
+            Configure();
+        }
 
         public KongrevskyDbContext(IServiceProvider serviceProvider, DbConnection existingConnection, DbCompiledModel model, bool contextOwnsConnection)
-                : base(serviceProvider, existingConnection, model, contextOwnsConnection) { Configure(); }
+                : base(serviceProvider, existingConnection, model, contextOwnsConnection)
+        {
+            Configure();
+        }
 
         private void Configure()
         {
